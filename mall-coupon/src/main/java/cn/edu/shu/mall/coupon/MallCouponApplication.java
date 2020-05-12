132e511a197b0e4aa4b0ab01c3f9c0ad31d84b93
lloyd <lloyd@shu.edu.cn>
2020-05-12 17:10:01 +0800

    5_12commit

---
+++ mall-coupon/src/main/java/cn/edu/shu/mall/coupon/MallCouponApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.mall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponApplication.class, args);
+    }
+
+}
---
+++ mall-product/src/test/java/cn/edu/shu/mall/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.mall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
